783efd94fdedaaa499215d303d6312725cd35416
isbnn <isb.cv@yandex.ru>
2019-05-21 23:48:35 +0300

    21.05.2019

---
+++ redux_form/src/App/index.js
@@ -0,0 +1,34 @@
+import React, {Component, Fragment} from 'react';
+import LoginForm from '../components/LoginForm';
+import {SubmissionError} from 'redux-form';
+
+class App extends Component {
+  
+  submit = input=>{
+    if(['Настя', 'Леша', 'Миша', 'Света'].includes (input.username)){
+      throw new SubmissionError ({
+        username : "Имя пользователя уже существует",
+      });
+    }else{
+      window.alert (JSON.stringify(input))
+    }
+};
+
+  getInitialValues () {
+    return {
+      username: 'Юрчик',
+      password: '',
+    };
+  }
+  
+  render () {
+    return (
+      <Fragment>
+        <h1>Redux Form</h1>
+        <LoginForm onSubmit={this.submit} initialValues={this.getInitialValues()}/>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
